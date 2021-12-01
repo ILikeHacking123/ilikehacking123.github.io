dec97501e253ebed74e2c13efd09f1b1efa3c64b
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-12-01 11:08:06 +0700

    Create deco.js

---
+++ deco.js
@@ -0,0 +1,9 @@
+/* TuanminhDev.GitHub.Io decoration and style file 
+
+Tuanminh-OS-like homepage but it simple than
+*/
+
+function colorSet() {
+  document.body.style.backgroundColor = "#252545";
+}
+colorSet();
