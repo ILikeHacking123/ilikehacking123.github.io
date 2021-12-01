4c452074e15855f5cad0e3d143fa4b4e7042c065
Tuanminh batch developer <89622068+TuanminhDev@users.noreply.github.com>
2021-12-01 14:28:46 +0700

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,15 +1,32 @@
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-  }
-  function bannedChecker() {
-    let BanRefreshActi = getCookie("banned");
-    if (BanRefreshActi == "1") {
-      alert("You are banned.");
-    } else {
-      window.location.replace("https://tuan-os.github.io/home");
-      alert("Welcome");
+    function setCookie(cname,cvalue,exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires=" + d.toGMTString();
+        document.cookie = cname+"="+cvalue+"; "+expires;
     }
-  }
-  bannedChecker(); // check cookies
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return null;
+    }
+
+    function checkCookie() {
+        var user=getCookie("username");
+         if (user != null) {
+            document.getElementById("person").innerHTML =
+            "Hello " + user + "!";
+        } else {
+           user = prompt("What your name?","");
+           if (user != "" && user != null) {
+               setCookie("username", user, 90);
+           }
+        }
+    }
